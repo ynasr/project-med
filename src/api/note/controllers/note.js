bb0ce087601778cdd01533df7219424f22ebc210
Yazen Nasr <ynasr@Yazens-MBP.lan>
2022-12-08 22:02:42 -0800

    Updated schema and build prompt off of req body inputs

--- src/api/note/controllers/note.js
+++ src/api/note/controllers/note.js
@@ -9,7 +9,8 @@ const gptGenerator = require("../../../helpers/gptGenerator");
 
 module.exports = createCoreController('api::note.note', ({ strapi }) => ({
   async gptGenerate(ctx) {
-    const { prompt } = ctx.request.body;
+    const { sex, age, unit, condition, history } = ctx.request.body;
+    const prompt = `Example assessment and treatment plans for ${sex} patient aged ${age} ${unit} diagnosed with ${condition} and with a pertinent medical history of ${history.join(',')}`;
     console.log('before call...')
     const patientNote = await gptGenerator.generatePatientNote(prompt);
     console.log(`PATIENT NOTE: ${patientNote}`);
